4544b5452df993bbea0daaaf1e6453560557f04c
Sergio Hernandez <alejo.80.123@gmail.com>
2024-08-07 18:25:05 -0500

    init

---
+++ src/main/java/com/challenge/ecommercebackend/EcommerceBackEndApplication.java
@@ -0,0 +1,12 @@
+package com.challenge.ecommercebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceBackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcommerceBackEndApplication.class, args);
+    }
+}
